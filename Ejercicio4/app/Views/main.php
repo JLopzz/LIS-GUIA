1a509d07898718cafa4394bbd45f186b3e66a0d3
JLopzz <javylopzz@gmail.com>
2020-04-10 15:41:36 -0600

    db

--- Ejercicio4/app/Views/main.php
+++ Ejercicio4/app/Views/main.php
@@ -7,32 +7,34 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Main</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-  <h1>Main Page</h1>
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Titulo</th>
-      <th scope="col">Duracion</th>
-      <th scope="col">Genero</th>
-      <th scope="col">Director</th>
-      <th scope="col">Descripcion</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach($pelicula as $k => $p): ?>
-    <tr>
-      <th><?= $p['titulopelicula'] ?></th>
-      <td><?= $p['duracion'] ?></td>
-      <td><?= $match->getGen($p['idgenero']) ?></td>
-      <td><?= $match->getNameDir($p['iddirector']) ?></td>
-      <td><?= $p['descripcion'] ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+  <div class='container'>
+    <h1>Peliculas en la Base de Datos</h1>
+    <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Titulo</th>
+        <th scope="col">Duracion</th>
+        <th scope="col">Genero</th>
+        <th scope="col">Director</th>
+        <th scope="col">Descripcion</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach($pelicula as $k => $p): ?>
+      <tr>
+        <th><?= $p['titulopelicula'] ?></th>
+        <td><?= $p['duracion'] ?></td>
+        <td><?= $match->getGen($p['idgenero']) ?></td>
+        <td><?= $match->getNameDir($p['iddirector']) ?></td>
+        <td><?= $p['descripcion'] ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+  </div>
 </body>
 </html>
