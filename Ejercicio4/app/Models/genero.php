a01d937c96f047e8e43316c96d1f88fd59401a2a
JLopzz <javylopzz@gmail.com>
2020-04-20 12:47:57 -0600

    Comentarios de funciones

--- Ejercicio4/app/Models/genero.php
+++ Ejercicio4/app/Models/genero.php
@@ -1,22 +1,54 @@
 <?php namespace App\Models;
 
+/**
+ * Genero
+ * 
+ * Clase extiende de Model
+ * Nos devuelve las funciones para trabajo con la base de datos,
+ * y funciones de CRUD para la tabla Genero de la base de datos
+ */
+
 use CodeIgniter\Model;
 
 class Genero extends Model
 {
+
+  //Tabla con la que se trabajara
   protected $table      = 'genero';
+
+  //Llave primaria de la tabla seleccionada
   protected $primaryKey = 'ididgenero';
 
+  /*
+   * Manera en que se devuelven los datos
+   * Puede devolver array u objeto
+   */
   protected $returnType = 'array';
+
+  //Borrado para el aplicativo o para la base de datos
   protected $useSoftDeletes = false;
 
+  //Campos de la tabla con los que se podra insertar
   protected $allowedFields = ['generopelicula'];
 
+  /**
+   * funciones extras que no se ocupan
+   * sobre almacenamiento de fechas en la tabla:
+   *  -Fecha de Creacion
+   *  -Fecha de Actualizacion
+   *  -Fecha de Eliminacion
+   */
   protected $useTimestamps = false;
   protected $createdField  = '';
   protected $updatedField  = '';
   protected $deletedField  = '';
 
+  /**
+   * Funciones de validacion en aplicativo
+   *  -Reglas de Validacion por campo de la Tabla
+   *  -Mensajes que apareceran al no cumplirse las reglas de Validacion
+   *  -Saltar/ignorar validacion
+   */
   protected $validationRules    = [];
   protected $validationMessages = [];
   protected $skipValidation     = false;
--- Ejercicio4/app/Models/pelicula.php
+++ Ejercicio4/app/Models/pelicula.php
@@ -1,26 +1,58 @@
 <?php namespace App\Models;
 
+/**
+ * Pelicula
+ * 
+ * Clase extiende de Model
+ * Nos devuelve las funciones para trabajo con la base de datos,
+ * y funciones de CRUD para la tabla Pelicula de la base de datos
+ */
+
 use CodeIgniter\Model;
 
 class Pelicula extends Model
 {
+  
+  //Tabla con la que se trabajara
   protected $table      = 'pelicula';
+  
+  //Llave primaria de la tabla seleccionada
   protected $primaryKey = 'idpelicula';
 
+  /*
+   * Manera en que se devuelven los datos
+   * Puede devolver array u objeto
+   */
   protected $returnType = 'array';
+  
+  //Borrado para el aplicativo o para la base de datos
   protected $useSoftDeletes = false;
 
+  //Campos de la tabla con los que se podra insertar
   protected $allowedFields = [
     'titulopelicula', 'descripcion', 'imgpelicula',
     'titulooriginal', 'duracion', 'idgenero',
     'iddirector'
   ];
 
+  /**
+   * funciones extras que no se ocupan
+   * sobre almacenamiento de fechas en la tabla:
+   *  -Fecha de Creacion
+   *  -Fecha de Actualizacion
+   *  -Fecha de Eliminacion
+   */
   protected $useTimestamps = false;
   protected $createdField  = '';
   protected $updatedField  = '';
   protected $deletedField  = '';
 
+  /**
+   * Funciones de validacion en aplicativo
+   *  -Reglas de Validacion por campo de la Tabla
+   *  -Mensajes que apareceran al no cumplirse las reglas de Validacion
+   *  -Saltar/ignorar validacion
+   */
   protected $validationRules    = [];
   protected $validationMessages = [];
   protected $skipValidation     = false;
