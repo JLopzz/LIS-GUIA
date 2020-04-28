2920e5200bc9e9decf290730d678a1155be2799d
SalvadorPerz <63437250+SalvadorPerz@users.noreply.github.com>
2020-04-27 20:10:21 -0600

    Update content.php

--- Ejercicio3/content.php
+++ Ejercicio3/content.php
@@ -14,24 +14,30 @@
 
 		exit(0);
 	}
+	if (!$cn->set_charset("utf8")) {
+			printf("Error cambiando el juego de caracteres utf8: %s\n", $cn->error);
+		}else {
+	
 
 	$tipo = isset($_GET['content']) ? $_GET['content'] : 0;
 	if ($tipo != 0) {
 		$qr = "SELECT nombrelenguaje, descripcion, imglenguaje FROM lenguaje ";
 		$qr .= "WHERE idlen = $tipo";
 		$rs = $cn->query($qr);
+		
+		
 
 		while ($row = $rs->fetch_object()) {
-			$info = "<div id=\"nombre\">\n<h2>{$row -> nombrelenguaje}</h2>\n</div>\n";
+			$info  = "<div id=\"nombre\">\n<h2>{$row -> nombrelenguaje}</h2>\n</div>\n";
 			$info .= "<div id=\"descripcion\">\n<p>\n{$row -> descripcion}\n";
 			$info .= "<img src=\"{$row -> imglenguaje}\n\" />\n";
 			$info .= "</p>\n</div>\n";
 		}
 
-		if (!$cn->set_charset("utf8")) {
-			printf("Error cambiando el juego de caracteres utf8: %s\n", $cn->error);
-		}else {
+		
 			printf($info, $cn->character_set_name());
 		}
+		
+		
 	}
 ?>
