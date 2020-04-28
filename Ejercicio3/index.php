fb041dc864fb6f637007cafec08e1756a7fc00c3
JLopzz <javylopzz@gmail.com>
2020-04-27 23:50:25 -0600

    looking dirs

--- Ejercicio3/index.php
+++ Ejercicio3/index.php
@@ -1,35 +1,35 @@
-<!DOCTYPE html>
+	<!DOCTYPE html>
 	<html lang="es">
-		<head>
-			<meta http-equiv = "Content-type" content = "text/html; charset=utf-8" />
-			<title>Lenguajes de Programación</title>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv = "Content-type" content = "text/html; charset=utf-8" />
+		<title>Lenguajes de Programación</title>
+		<script src = "js/ahahLib.js" ></script>
+		<script src = "js/activetab.js" ></script>
+		<link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity = "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin = "anonymous" >
+		<link rel="stylesheet" href="css/estilo.css"></link>
+	</head>
 
-			<script src = "js/ahahLib.js" ></script>
-			<script src = "js/activetab.js" ></script>
-			<link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity = "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin = "anonymous" >
-			<link rel="stylesheet" href="css/estilo.css"></link>
-		</head>
+	<body class="container">
+		<section>
+			<article>
+				<ul id="tabmenu" class="nav nav-tabs mt-5">
+					<li id="t1" class="nav-item" onclick="makeactive(1)">
+						<a class="nav-link active" id="tab1">PHP</a>
+					</li>
 
-		<body class="container">
-			<section>
-				<article>
-					<ul id="tabmenu" class="nav nav-tabs mt-5">
-						<li id="t1" class="nav-item" onclick="makeactive(1)">
-							<a class="nav-link active" id="tab1">PHP</a>
-						</li>
+					<li id="t2" class="nav-item" onclick="makeactive(2)">
+						<a class="nav-link" id="tab2">JAVA</a>
+					</li>
 
-						<li id="t2" class="nav-item" onclick="makeactive(2)">
-							<a class="nav-link" id="tab2">JAVA</a>
-						</li>
-
-						<li id="t3" class="nav-item" onclick="makeactive(3)">
-							<a class="nav-link" id="tab3">C#</a>
-						</li>
-					</ul>
-					<div id="content">
-						(Aquí se mostrará el contenido de cada pestaña)
-					</div>
-				</article>
-			</section>
-		</body>
+					<li id="t3" class="nav-item" onclick="makeactive(3)">
+						<a class="nav-link" id="tab3">C#</a>
+					</li>
+				</ul>
+				<div id="content">
+					(Aquí se mostrará el contenido de cada pestaña)
+				</div>
+			</article>
+		</section>
+	</body>
 	</html>
